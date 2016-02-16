f484cb107d9112c721804f2c1d0929a07089ef29
Mingqian Wang <mingqianwang96@gmail.com>
2016-02-16 23:08:08 +0000

    convert GET/todo/:id to use sequelize

--- server.js
+++ server.js
@@ -39,15 +39,16 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {　
   var todoId = parseInt(req.params.id, 10);
-  var mactchTodo = _.findWhere(todos, {
-    id: todoId
-  });
 
-  if (mactchTodo) {
-    res.json(mactchTodo);
-  } else {
-    res.status(404).send();
-  }
+  db.todo.findById(todoId).then(function (todo){
+    if(!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).send();
+    }
+  }, function (e) {
+    res.status(500).send();
+  });
 });
 
 app.post('/todos', function(req, res) {
